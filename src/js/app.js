6dc36a0ee1b19f4f2aa0ae0ada6b7258651eb711
explt13 <az13rede4d@gmail.com>
2024-02-09 00:22:59 +0600

    v

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,13 @@
+import $ from 'jquery'
+window.$ = $;
+
 import * as functions from './modules/functions.js';
 import { ibg } from './modules/ibg.js';
 import { burgerMenu } from './modules/burgermenu.js'
 import { videos } from './modules/videos.js';
 import { contactCardAnim } from './modules/contact-card-anim.js';
+import { showContent } from './modules/showContent.js';
+import { slickSlider } from './modules/slick-slider.js';
 
 functions.isWebp()
 
@@ -11,5 +16,7 @@ $(document).ready(function(){
     burgerMenu()
     videos()
     contactCardAnim()
+    showContent()
+    slickSlider()
 })
 
--- src/js/modules/slick-slider.js
+++ src/js/modules/slick-slider.js
@@ -1,65 +1,136 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        arrows: true,
-        dots: true,
-        adaptiveHeight: true, // use align-items: flex-start on slick-track
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 1000,
-        easing: 'ease',
-        infinite: true,
-        initialSlide: 0,
-        autoplay: false,
-        autoplaySpeed: 1500,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDotHover: true,
-        draggable: true,
-        swipe: true,
-        touchThreshold: 10,
-        touchMove: true,
-        waitForAnimate: true,
-        centerMode: true,
-        variableWidth: true,
-        rows: 1,
-        slidesPerRow: 1,
-        vertical: false, // use display block on slick track
-        verticalSwiping: false,
-        asNavFor: ".sliderBig", // must have same elements and quantity
-        responsive: [
-            {
-                breakpoint: 768, // max-width x < 768
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 480, // max-width x < 480
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ],
-        mobileFirst: false, // change breakpoint to min-width
-        // appendArrows: $('content'), // move arrows to some element
-        // appendDots: $('content')
+import 'slick-slider'
+export const slickSlider = () => {
+    const setSlider = function(){
+        const mql = window.matchMedia('(max-width: 600px)')
+        if (mql.matches){
+            $('.achievements__row_cards').addClass('slider-achievements')
+            // $('.achievements__row_cards').children().addClass('slider-item') for row porps
+            $('.slider-achievements').slick({
+                arrows: false,
+                dots: true,
+                adaptiveHeight: false, // use align-items: flex-start on slick-track
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 1000,
+                easing: 'ease',
+                infinite: true,
+                initialSlide: 0,
+                autoplay: true,
+                autoplaySpeed: 1500,
+                pauseOnFocus: true,
+                pauseOnHover: true,
+                pauseOnDotHover: true,
+                draggable: true,
+                swipe: true,
+                touchThreshold: 10,
+                touchMove: true,
+                waitForAnimate: true,
+                centerMode: true,
+                variableWidth: false,
+                rows: 0, // creates empty div
+                slidesPerRow: 1,
+                vertical: false, // use display block on slick track
+                verticalSwiping: false,
+                
+            });
 
+            $('.advice__row_block').addClass('slider-advice').children().addClass('slider-advice__item')
+      
+            $('.slider-advice').slick({
+                arrows: false,
+                dots: true,
+                adaptiveHeight: false, // use align-items: flex-start on slick-track
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 1000,
+                easing: 'ease',
+                infinite: true,
+                initialSlide: 0,
+                autoplay: false,
+                autoplaySpeed: 1500,
+                pauseOnFocus: true,
+                pauseOnHover: true,
+                pauseOnDotHover: true,
+                draggable: true,
+                swipe: true,
+                touchThreshold: 10,
+                touchMove: true,
+                waitForAnimate: true,
+                centerMode: true,
+                variableWidth: false,
+                rows: 0,
+                slidesPerRow: 1,
+                vertical: false, // use display block on slick track
+                verticalSwiping: false,
+                
+            });
 
-    });
+            $('.trusted__companies').addClass('slider-companies')
+            $('.slider-companies').slick({
+                arrows: false,
+                dots: false,
+                adaptiveHeight: false, // use align-items: flex-start on slick-track
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                speed: 5000,
+                // easing: 'ease',
+                cssEase: 'linear',
+                infinite: true,
+                initialSlide: 0,
+                autoplay: true,
+                autoplaySpeed: 0,
+                pauseOnFocus: false,
+                pauseOnHover: false,
+                pauseOnDotHover: false,
+                draggable: false,
+                swipe: false,
+                touchThreshold: 10,
+                touchMove: false,
+                waitForAnimate: false,
+                centerMode: true,
+                variableWidth: false,
+                rows: 0,
+                slidesPerRow: 0,
+                vertical: false, // use display block on slick track
+                verticalSwiping: false,
+            })
+        }
+    }
+    $(window).on('resize', setSlider)
+    setSlider()
+   
+    // verticalSwiping
+    // asNavFor: ".sliderBig", // must have same elements and quantity
+    // responsive: [
+    //     {
+    //         breakpoint: 768, // max-width x < 768
+    //         settings: {
+    //             slidesToShow: 2
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 480, // max-width x < 480
+    //         settings: {
+    //             slidesToShow: 1
+    //         }
+    //     }
+    // ],
+    // mobileFirst: false, // change breakpoint to min-width
+    // appendArrows: $('content'), // move arrows to some element
+    // appendDots: $('content')
+    // $('.sliderBig').slick({
+    //     arrows: false,
+    //     fade: true, // slide show -> without slide animation slidesToShow must be 1
+    //     asNavFor: ".slider"// must have same elements and quantity
+    // })
 
-    $('.sliderBig').slick({
-        arrows: false,
-        fade: true, // slide show -> without slide animation slidesToShow must be 1
-        asNavFor: ".slider"// must have same elements and quantity
-    })
+    // //EVENTS
+    // $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
 
-    //EVENTS
-    $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    // })
+    // $('.slider').on('afterChnage', function(event, slick, currentSlide){
 
-    })
-    $('.slider').on('afterChnage', function(event, slick, currentSlide){
-
-    })
+    // })
 
     //METHODS
     // $('.slider').slick('setPosition'); // when i need to reload file if display none than onclick -> display block and need to set position
@@ -90,4 +161,5 @@ $(document).ready(function(){
     // let option = $('.slider').slick('slickGetOption', 'slidesToShow');
     // $('.slider').slick('slickSetOption', 'slidesToShow', 2);
     // $('.slider').slick('unslick')
-});
+}
+    
