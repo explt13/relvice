7fd0572b46dd5881546735f76b7a426150e895c4
explt13 <az13rede4d@gmail.com>
2024-02-09 16:51:54 +0600

    fv

--- src/js/modules/ibg.js
+++ src/js/modules/ibg.js
@@ -1,8 +1,8 @@
 export const ibg = () => {
     $.each($('.ibg'), function(index, val) {
         if ($(this).find('img').length > 0){
-            if ($('body').hasClass('webp1')){ // for dev purpuses 1
-                $(this).css('background-image', 'url("' + $(this).find('img').attr('src').replace(/\.\w+$/, '.webp') + '")')
+            if ($('body').hasClass('webp')){ // for dev purpuses 1
+                $(this).css('background-image', 'url("' + $(this).find('source').attr('srcset') + '")')
             } else {
                 $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")')
             }
@@ -12,4 +12,3 @@ export const ibg = () => {
         
     })
 }
-    
